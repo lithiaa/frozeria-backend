bc77d3337e487f957300aa7e372d13dcae87c039
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2026-05-08 22:46:08 +0700

    feat: prisma configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { ItemModule } from './item/item.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    CategoriesModule,
+    ItemModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
