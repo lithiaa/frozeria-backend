11fa50f8e254e0e015256e854450340d1a4f6d64
Muhammad Bagus Indrawan <100072786+lithiaa@users.noreply.github.com>
2026-05-08 22:56:14 +0700

    feat: add CRUD API Categories

---
+++ src/categories/categories.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Post, Param, Patch, ParseIntPipe, Delete } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+@Controller('categories')
+export class CategoriesController {
+  constructor(private readonly categoriesService: CategoriesService) {}
+
+  @Post()
+  create(@Body() createCategoryDto: CreateCategoryDto) {
+    return this.categoriesService.create(createCategoryDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return this.categoriesService.update(id, updateCategoryDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.remove(id);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.categoriesService.findOne(id);
+  }
+
+  @Get()
+  findAll() {
+    return this.categoriesService.findAll();
+  }
+}
---
+++ src/categories/categories.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+
+@Injectable()
+export class CategoriesService {
+
+  constructor(private prisma: PrismaService) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    return this.prisma.category.create({
+      data: createCategoryDto,
+    });
+  }
+
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category not found`);
+    }
+
+    return this.prisma.category.update({
+      where: { id },
+      data: updateCategoryDto,
+    });
+  }
+
+  async remove(id: number) {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+    });
+
+    if (!category) {
+      throw new NotFoundException(`Category not found`);
+    }
+
+    return this.prisma.category.delete({
+      where: { id },
+    });
+  }
+
+  async findOne(id: number) {
+    return this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.category.findMany();
+  }
+}
